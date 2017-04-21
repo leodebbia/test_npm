15b40dcc0a523100a792b65f372d28d42f8806ac
4f06 <4f06@Liceospalla.local>
2017-04-21 09:13:07 +0200

    ciao

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 'use strict';
 const express = require('express');
 const app = new express();
-var x = 0;
+
 
 app.listen(8000, () =>{
 	console.log("Server in ascolto sulla porta 8000");
 });
 
 app.get('/', (req,res) => {
-	res.send('Homepage');
+	res.send('CIAONE');
 });
 
 app.get('/bella', (req,res) => {
-	res.send('Site created by Zanni');
+	res.send('Leodebbia was here');
 });
 
 app.get('*', (req,res) => {
-	res.status(404).send('error (404)');
+	res.status(404).send('error 404');
 });
 
